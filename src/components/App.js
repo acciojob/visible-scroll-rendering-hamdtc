8eb111970ef46b45478485ebbb9bbaebf656e966
Mohamed t c <97926192+hamdtc@users.noreply.github.com>
2023-04-26 09:39:54 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,10 @@
-
 import React from "react";
 import './../styles/App.css';
+import List from "./List";
 
 const App = () => {
   return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
+    <List />
   )
 }
 
